8a69a5b92845fb1c05d75d4bd5df17aa29fe1587
Felix DB <felixjtdb@gmail.com>
2018-09-25 14:47:16 +0000

    VD students in array

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
 student_count = 11
 
+students = [                                # Array of students
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
-puts "The students of Villains Academy" # Give the header then follow with a 
-puts "-------------"                    # list of the students
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts "The students of Villains Academy"     # Header before list
+puts "-------------"
+
+students.each do |x|                        # iterate through the array of 
+  puts x                                    # students and print 
+end
 
 # Print the number of students
-puts "Overall, we have #{student_count} great students" 
+puts "Overall, we have #{student_count} great students!" 
