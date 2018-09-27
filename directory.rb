8878da4dc32548c8587740d68ec667aaf3d0fc5c
Felix DB <felixjtdb@gmail.com>
2018-09-27 18:48:22 +0000

    interactive menu refactored (step10-done)

--- directory.rb
+++ directory.rb
@@ -1,32 +1,64 @@
-students = [                                # Array of students
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
+@students = Array.new
 
 def print_header
   puts "The students of Villains Academy"     # Header before list
   puts "-------------"
 end
 
-def print_students(names)
-  names.each do |x|                        # iterate through the array of 
-    puts x                                 # students and print 
+def print_students_list
+  @students.each_with_index do |info, idx|                 # print name & cohort 
+    puts "#{idx + 1}. #{info[:name]} (#{info[:cohort]} cohort)"  
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students!"   # Print the number
+def print_footer
+  puts "Overall, we have #{@students.count} great students!" # Print the number
 end                                                          # of students
 
-print_header
-print_students(students)
-print_footer(students)
+def input_students
+  puts "Please enter the names of the studnets"
+  puts "To finsih press RETURN twice"                             
+  
+  name = gets.chomp
+  while !name.empty? do                  # Loop until enter is pressed twice
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+
+interactive_menu
+
